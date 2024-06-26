287ba2c4c8a9869a78f592e4057c8bd80368e76a
Thales Rangel <thalesran2006@gmail.com>
2024-06-26 00:26:52 -0300

    Começo do Back End
    - Comecei a criar os comandos que definirão como será o Banco de Dados

---
+++ Projeto/Models/Database.py
@@ -0,0 +1,60 @@
+import sqlite3
+from PIL import Image
+
+
+class Banco:
+    def __init__(self):
+        self.conect = sqlite3.connect('database.db')
+
+        self.cur = self.conect.cursor()
+
+        self.cur.execute("""
+            create table if not exists artistas(
+                nome text primary key,
+                foto blob
+            )
+        """)
+
+        self.cur.execute("""
+            create table if not exists albuns(
+                nome text primary key,
+                foto blob
+            )
+        """)
+
+        self.cur.execute("""
+            create table if not exists musicas(
+                nome text primary key,
+                minutos int,
+                segundos int,
+                album text,
+                artista text,
+                caminho text,
+                foreign key (album) references albuns(nome),
+                foreign key (artista) references artistas(nome)
+            )
+        """)
+
+        self.cur.execute("""
+            create table if not exists playlists(
+                nome text primary key
+            )
+        """)
+
+        self.cur.execute("""
+            create table if not exists na_playlist(
+                id integer primary key autoincrement,
+                musica text,
+                playlist text,
+                foreign key (musica) references musicas(nome),
+                foreign key (playlist) references playlists(nome)
+            )
+        """)
+
+        try:
+            self.cur.execute("insert into playlists values('Favoritas')")
+            self.conect.commit()
+        except sqlite3.IntegrityError:
+            print("Playlist de favoritas já criada!")
+
+        self.conect.close()
